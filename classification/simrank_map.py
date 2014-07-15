c0a2f1a59583bf10f2b365d42434b8c2feab2f6e
Heming Keh <heming.keh@gmail.com>
2014-07-14 20:00:42 -0700

    comment

--- classification/simrank_map.py
+++ classification/simrank_map.py
@@ -27,11 +27,18 @@ def main():
 
     for row in rows:
       try:
-        # Add the product to the sum
+        # v[col]: 表示当前col结点的享有的访问概率，col中存储值即可能是用户ID，
+        #   也可能是Content ID
+        # len(rows): 表示col结点的outcoming links的数量，1/len(rows)也就是走
+        #    'col -> row' 这条边的概率。
+
+        # 这段代码看上去好像用矩阵M的每列，去和向量distribution vector做点积，
+        # 其实不然，这里相乘完之后，以row为key把数据交给reducer处理，
+        # reducer收到的数据是自动排序过的，这样所有同row的数据会聚合在一起，
+        # 再相加，其实还是相当于用M的每行去和v做点积。
         print "%s\t%.20f" % (row, v[col] / float(len(rows)))
       except KeyError:
         # KeyError equates to a zero, which we don't need to output.
-        # 这里其实把所有的没有out-links的结点都给丢弃了
         pass
 
 if __name__ == '__main__':
